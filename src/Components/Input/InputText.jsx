decce60a579b33e88e40e0290bfe14bae4432c85
sunz8281 <sunz8281@gmail.com>
2024-10-11 15:04:51 +0900

    add Radio

--- src/Components/Input/InputText.jsx
+++ src/Components/Input/InputText.jsx
@@ -10,17 +10,14 @@ export default function InputText({ placeholder }) {
 
 const Input = styled.input`
     width: 100vw;
+    height: 60px;
     padding: 17px 30px;
     border-radius: 10px;
     border: 2px solid #B9B7B7;
     font-size: 15px;
     font-weight: 700;
     letter-spacing: -0.33px;
-    &:focus {
-        outline: none;
-    }
     &::placeholder {
         color: #B9B7B7;
-        
     }
 `
---
+++ src/Components/Input/Radio.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export default function Radio({text, id, name, checked}) {
+    return (
+        <div>
+            <RealRadio type={"radio"} id={id} name={name} defaultChecked={checked}/>
+            <CustomRadio htmlFor={id}>{text}</CustomRadio>
+        </div>
+    );
+}
+
+const RealRadio = styled.input`
+    display: none;
+`
+
+const CustomRadio = styled.label`
+    border-radius: 10px;
+    width: 100vw;
+    height: 60px;
+    border: 1px solid #B9B7B7;
+    color: #B9B7B7;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 15px;
+    cursor: pointer;
+    ${RealRadio}:checked + & {
+        border: 3px solid #FF8024;
+        color: #FF8024;
+        font-weight: 800;
+    }
+`
