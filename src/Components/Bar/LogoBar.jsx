a47cecdd8bb31f5663ef002e02d23c1b378155ad
sunz8281 <sunz8281@gmail.com>
2024-10-12 23:37:04 +0900

    add LogoBar

---
+++ src/Components/Bar/LogoBar.jsx
@@ -0,0 +1,20 @@
+import LogoOrange from '../../Assets/Logos/logo-orange.svg';
+import styled from "styled-components";
+
+export default function LogoBar() {
+    return (
+        <Container>
+            <Logo src={LogoOrange}/>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: end;
+`
+const Logo = styled.img`
+    height: 50%;
+`
