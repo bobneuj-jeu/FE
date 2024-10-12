1e20624de9df57f0e18f4d4a8256ee5c326df2e0
sunz8281 <sunz8281@gmail.com>
2024-10-12 23:31:53 +0900

    add TopBar

---
+++ src/Components/Bar/TopBar.jsx
@@ -0,0 +1,24 @@
+import title from '../../Assets/Logos/title-orange.svg';
+import styled from "styled-components";
+
+export default function TopBar() {
+    return (
+        <Container>
+            <Logo src={title} alt="Logo" />
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    width: 100%;
+    height: 100px;
+    border-bottom: 1px solid #444444;
+    position: relative;
+`
+
+const Logo = styled.img`
+    position: absolute;
+    height: 25%;
+    bottom: 15px;
+    left: 15px;
+`
