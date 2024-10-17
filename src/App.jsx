ff6440f8ecedae4c5ffb47f2dc8a3726e0916a67
sunz8281 <sunz8281@gmail.com>
2024-10-17 13:23:19 +0900

    set Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 import {Routes, Route} from 'react-router-dom'
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Home from "./Pages/Home";
+import Menu from "./Pages/Menu";
+import Fridge from "./Pages/Fridge";
+import MakeMenu from "./Pages/MakeMenu";
+import User from "./Pages/User";
+import {useState} from "react";
+import NavBar from "./Components/Bar/NavBar";
+import styled from "styled-components";
 
 function App() {
+    const [isLogin, setIsLogin] = useState(true);
     return (
-        <Routes>
+        <Container className={"App"}>
+            {isLogin ? (
+                <>
+                    <Routes>
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/home" element={<MakeMenu />} />
+                        <Route path="/menu" element={<Menu />} />
+                        <Route path="/fridge" element={<Fridge />} />
+                        <Route path="/user" element={<User />} />
+                    </Routes>
+                    <NavBar/>
+                </>
+            ) : (
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                </Routes>
+            )}
+        </Container>
 
-        </Routes>
     );
 }
+
+const Container = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+`
 
 export default App;
